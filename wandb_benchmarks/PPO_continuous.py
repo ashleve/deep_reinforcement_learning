88c1dd892a78352d73dd3af908da5f72fc9f1cba
hobogalaxy <zalewski.ukas@gmail.com>
2020-09-06 22:53:14 +0200

    Improved PPO_continuous.py

--- wandb_benchmarks/PPO_continuous.py
+++ wandb_benchmarks/PPO_continuous.py
@@ -6,17 +6,17 @@ from torch.utils.data import SubsetRandomSampler, BatchSampler
 from torch.optim import Adam
 from torch.distributions import Normal
 import wandb
-# import numpy as np
-# import pybullet_envs
+import numpy as np
+import pybullet_envs
 import time
 
 
-wandb.init(project="PPO_continuous2", group="lunar_batch_256_mem_4096_epochs_10_grad_clip", job_type="eval")
+wandb.init(project="PPO_continuous2", group="walker", job_type="eval")
 
 config = wandb.config
 # config.env_name = 'LunarLander-v2'
-config.env_name = 'LunarLanderContinuous-v2'
-# config.env_name = 'BipedalWalker-v3'
+# config.env_name = 'LunarLanderContinuous-v2'
+config.env_name = 'BipedalWalker-v3'
 # config.env_name = 'MountainCarContinuous-v0'
 # config.env_name = 'MinitaurBulletEnv-v0'
 # config.env_name = 'HumanoidBulletEnv-v0'
@@ -32,15 +32,16 @@ config.env_name = 'LunarLanderContinuous-v2'
 env = gym.make(config.env_name)
 
 config.gamma = 0.99
-config.batch_size = 256
-config.memory_size = 4096
+config.lamb = 0.95
+config.batch_size = 64
+config.memory_size = 2048
 config.hidden_size = 64
 config.actor_lr = 0.0003
 config.critic_lr = 0.0003
 config.ppo_multiple_epochs = 10
 config.eps = 0.2
 config.grad_clip_norm = 0.5
-config.entropy_weight = 0.01
+config.entropy_weight = 0.0
 config.max_timesteps = 1500
 
 config.obs_space = env.observation_space.shape[0]
@@ -53,6 +54,10 @@ print(config.action_space)
 print(env.action_space.high)
 print(env.action_space.low)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+config.device = device.type
+print(device)
+
 
 class Memory:
     def __init__(self):
@@ -62,6 +67,8 @@ class Memory:
         self.done = []
         self.log_probs = []
         self.size = 0
+        self.last_state_for_gae = None
+        self.last_done_for_gae = None
 
     def add(self, state, action, reward, done, log_prob):
         self.states.append(state)
@@ -87,9 +94,9 @@ class Actor(nn.Module):
         self.actor = nn.Sequential(
             nn.Linear(config.obs_space, config.hidden_size),
             nn.Tanh(),
-            nn.Linear(config.hidden_size, 32),
+            nn.Linear(config.hidden_size, 64),
             nn.Tanh(),
-            nn.Linear(32, config.action_space),
+            nn.Linear(64, config.action_space),
             nn.Tanh()
         )
 
@@ -110,9 +117,9 @@ class Critic(nn.Module):
         self.critic = nn.Sequential(
             nn.Linear(config.obs_space, config.hidden_size),
             nn.Tanh(),
-            nn.Linear(config.hidden_size, 32),
+            nn.Linear(config.hidden_size, 64),
             nn.Tanh(),
-            nn.Linear(32, 1)
+            nn.Linear(64, 1)
         )
 
     def forward(self, state):
@@ -120,11 +127,30 @@ class Critic(nn.Module):
         return torch.squeeze(v)
 
 
-actor = Actor()
-critic = Critic()
+actor = Actor().to(device)
+critic = Critic().to(device)
 optimizer_actor = Adam(actor.parameters(), lr=config.actor_lr)
 optimizer_critic = Adam(critic.parameters(), lr=config.critic_lr)
 memory = Memory()
+
+
+def compute_GAE(rewards, state_values, done, gamma, lamb):
+    """
+        Computes Generalized Advantage Estimations for a single trajectory.
+        Params:
+            gamma - Discount.
+    """
+    returns = []
+    adv = []
+    running_sum = 0
+    state_values.append(0)
+    for i in reversed(range(len(rewards))):
+        delta = rewards[i] + gamma * state_values[i+1] * done[i+1] - state_values[i]
+        running_sum = delta + gamma * lamb * running_sum * done[i+1]
+        returns.insert(0, running_sum + state_values[i])
+        adv.insert(0, running_sum)
+
+    return returns, adv
 
 
 def compute_r2g(rewards, done, gamma):
@@ -143,6 +169,9 @@ def compute_r2g(rewards, done, gamma):
 
 
 def compute_loss(states, actions, rewards_to_go, adv, old_log_probs):
+
+    # adv = (adv - adv.mean()) / (adv.std() + 1e-5)
+    # rewards_to_go = (rewards_to_go - rewards_to_go.mean()) / (rewards_to_go.std() + 1e-5)
 
     # compute critic loss
     v = critic(states)
@@ -169,17 +198,24 @@ def update():
     rewards_to_go = compute_r2g(memory.rewards, memory.done, gamma=config.gamma)
 
     # prepare data
-    states = torch.squeeze(torch.stack(memory.states), 1).detach()
-    actions = torch.squeeze(torch.stack(memory.actions), 1).detach()
-    old_log_probs = torch.squeeze(torch.stack(memory.log_probs), 1).detach()
-    rewards_to_go = torch.tensor(rewards_to_go).detach()
-    state_values = critic(states).detach()
+    states = torch.squeeze(torch.stack(memory.states), 1).detach().to(device)
+    actions = torch.squeeze(torch.stack(memory.actions), 1).detach().to(device)
+    old_log_probs = torch.squeeze(torch.stack(memory.log_probs), 1).detach().to(device)
+    rewards_to_go = torch.tensor(rewards_to_go).detach().to(device)
+    state_values = critic(states).detach().to(device)
+
+    # compute GAE
+    _, adv = compute_GAE(memory.rewards, state_values.tolist(), memory.done, gamma=config.gamma, lamb=config.lamb)
+    # rewards_to_go = torch.tensor(returns).detach()    # DON'T
+    adv = torch.tensor(adv).detach().to(device)
 
     # normalize rewards-to-go
-    rewards_to_go = (rewards_to_go - rewards_to_go.mean()) / (rewards_to_go.std() + 1e-5)
+    # rewards_to_go = (rewards_to_go - rewards_to_go.mean()) / (rewards_to_go.std() + 1e-5)
+
+    # adv = (adv - adv.mean()) / (adv.std() + 1e-5)
 
     # compute advantage estimations
-    adv = (rewards_to_go - state_values)
+    # adv = (rewards_to_go - state_values)
 
     # learn
     for _ in range(config.ppo_multiple_epochs):
@@ -229,21 +265,20 @@ def update():
     })
 
 
-def train(num_of_games=10000, max_steps=10000):
+def train(max_num_of_games=100_000, max_steps=10_000, total_max_steps=1_000_000):
     total_number_of_steps = 0
 
-    for i in range(1, num_of_games):
+    for i in range(max_num_of_games):
         s = env.reset()
 
         total_ep_reward = 0
         for j in range(max_steps):
 
-            s = torch.FloatTensor(s.reshape(1, -1))
-            # s = torch.FloatTensor(s)
+            s = torch.FloatTensor(s.reshape(1, -1)).to(device)
 
             a, log_prob, pi = actor(s)
 
-            new_s, r, done, info = env.step(a.numpy().flatten())
+            new_s, r, done, info = env.step(np.clip(a.numpy().flatten(), config.action_low, config.action_high))
 
             done = 0 if done else 1
             memory.add(state=s, action=a, reward=r, done=done, log_prob=log_prob)
@@ -252,7 +287,9 @@ def train(num_of_games=10000, max_steps=10000):
             total_number_of_steps += 1
 
             if memory.size >= config.memory_size:
-                memory.rewards[-1] += config.gamma * critic(torch.FloatTensor(new_s)).item()  # bootstrap future reward
+                memory.done.append(done)  # this is just to make GAE computation work
+                memory.rewards[-1] += config.gamma * critic(torch.FloatTensor(new_s).to(device)).item() * done  # bootstrap future reward
+
                 update()
                 memory.clear()
 
@@ -261,7 +298,8 @@ def train(num_of_games=10000, max_steps=10000):
 
             s = new_s
 
-        print(f"Episode {i}, length: {j}")
+        if i % 10 == 0:
+            print(f"Episode {i}, length: {j}")
 
         wandb.log({
             "total number of steps": total_number_of_steps,
@@ -270,12 +308,15 @@ def train(num_of_games=10000, max_steps=10000):
             "episode length": j
         }, step=total_number_of_steps)
 
+        if total_number_of_steps > total_max_steps:
+            break
+
 
 def main():
     # wandb.watch(actor)
     # wandb.watch(critic)
 
-    train(num_of_games=1000000, max_steps=config.max_timesteps)
+    train(max_steps=config.max_timesteps, total_max_steps=2_000_000)
 
     # torch.save(actor_critic.state_dict(), "model.h5")
     # wandb.save('model.h5')
